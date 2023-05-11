4d08177f60be04365847e0310bd2915359f96b28
lbf-2974 <[lb_perso@hotmail.com](mailto:lb_perso@hotmail.com)>
2023-05-11 19:21:51 +0200

    for updates and timers

--- public/assets/scripts/timer.js
+++ public/assets/scripts/timer.js
@@ -1,9 +1,9 @@
 const departMinutes = 15;
-
 let temps = departMinutes * 60;
 
 const timerElement = document.getElementById('timer');
 
+function startTimer(){
 setInterval(function(){
     let minutes = parseInt(temps / 60, 10);
     let secondes = parseInt(temps % 60, 10);
@@ -14,5 +14,8 @@ setInterval(function(){
     timerElement.innerText = minutes + ':' + secondes;
     temps = temps <= 0 ? 0 : temps - 1;
 
-}, 1000)        
+}, 1000)}     
+
+const start = document.getElementById("start");
+start.addEventListener("click",startTimer );
 
