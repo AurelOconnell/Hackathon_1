7779a791a4d151a9008fc8975ba821cd0ed6f92b
AurelOconnell <aurelienoconnell@gmail.com>
2023-05-11 14:41:38 +0200

    DB enhanced

--- database.sql
+++ database.sql
@@ -1,91 +1,30 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
---
--- Client :  localhost
--- Généré le :  Jeu 26 Octobre 2017 à 13:53
--- Version du serveur :  5.7.19-0ubuntu0.16.04.1
--- Version de PHP :  7.0.22-0ubuntu0.16.04.1
+-- Adminer 4.8.1 MySQL 8.0.32 dump
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+SET NAMES utf8mb4;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+DROP DATABASE IF EXISTS `hackathon_1`;
+CREATE DATABASE `hackathon_1` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `hackathon_1`;
 
---
--- Base de données :  `simple-mvc`
---
+DROP TABLE IF EXISTS `kata`;
+CREATE TABLE `kata` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `titre` varchar(255) NOT NULL,
+  `énoncé` text NOT NULL,
+  `correction` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- --------------------------------------------------------
+INSERT INTO `kata` (`id`, `titre`, `énoncé`, `correction`) VALUES
+(1,	'Titre 1',	'ENONCE 1',	'Correction 1'),
+(2,	'Titre 2',	'ENONCE 2',	'Correction 2'),
+(3,	'Titre 3',	'ENONCE 3',	'Correction 3'),
+(4,	'Titre 4',	'ENONCE 4',	'Correction 4'),
+(5,	'Titre 5',	'ENONCE 5',	'Correction 5');
 
---
--- Structure de la table `item`
---
-
-CREATE TABLE `item` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `title` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Contenu de la table `item`
---
-
-INSERT INTO `item` (`id`, `title`) VALUES
-(1, 'Stuff'),
-(2, 'Doodads');
-
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `item`
---
-ALTER TABLE `item`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `item`
---
-ALTER TABLE `item`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-CREATE DATABASE hackathon_1;
-
-USE hackathon_1;
-
-CREATE TABLE kata (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-titre VARCHAR(255) NOT NULL, 
-énoncé TEXT NOT NULL,
-correction TEXT NOT NULL);
-
-INSERT INTO kata (titre, énoncé, correction) VALUES 
-('Titre 1', 
-'ENONCE 1',
-'Correction 1'
- ), 
- ('Titre 2', 
- 'ENONCE 2',
- 'Correction 2'),
- ('Titre 3',
- 'ENONCE 3',
- 'Correction 3'),
- ('Titre 4', 
- 'ENONCE 4',
- 'Correction 4'),
- ('Titre 5',
- 'ENONCE 5',
- 'Correction 5');
+-- 2023-05-11 12:39:43
