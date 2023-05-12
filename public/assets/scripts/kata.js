f72088f5ab0910af252def897462cffc7eff1995
maxdlr <105718327+maxdlr@users.noreply.github.com>
2023-05-12 13:50:28 +0200

    add gif to db

--- public/assets/scripts/kata.js
+++ public/assets/scripts/kata.js
@@ -1,12 +1,13 @@
-function updateKata(enonce, correction, titre) {
+function updateKata(enonce, correction, titre, gif) {
     document.getElementById('kata-instruction').innerHTML = enonce;
     document.getElementById('kata-answer').innerHTML = correction;
+    document.getElementById('kata-gif').src = gif;
     document.getElementById('kata-title').innerHTML = '"' + titre + '"';
 }
 
 document.getElementById('generator').addEventListener('click', function () {
     fetch('/api/kata/random')
     .then(response => response.json())
-    .then (data => updateKata(data.énoncé, data.correction, data.titre))
+    .then (data => updateKata(data.énoncé, data.correction, data.titre, data.gif))
     .catch(() => alert('error'));
 });
